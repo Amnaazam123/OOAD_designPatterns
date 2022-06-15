229070c7de56b248727002b6fbbba8630e4be886
Amna Azam <71166016+Amnaazam123@users.noreply.github.com>
2022-06-15 08:14:19 +0500

    Update strategyPattern.cs

--- ooad Patterns/strategyPattern.cs
+++ ooad Patterns/strategyPattern.cs
@@ -1,4 +1,4 @@
-
+//---------------------------------------------------------------------------- C# implementation-----------------------------------------------------------------------
 // In strategy pattern, class behavior can be changed at run time. It is more likely polymorphism :)
 // On run time, it is decided that which object will perform what?
 //the classes implementing strategies are also strategy.
@@ -74,5 +74,87 @@ namespace StrategyPattern
         }
 
     }
+}
+
+
+
+//----------------------------------------------------------------------- C++ implementation --------------------------------------------------------------------------
+
+#include <iostream>
+using namespace std;
+
+class Strategy
+{
+public:
+    virtual int doOperation(int num1, int num2) = 0;
+};
+class OperationAdd : public Strategy
+{
+public:
+    int doOperation(int num1, int num2)
+    {
+        return num1 + num2;
+    }
+};
+class OperationSubstract :public Strategy
+{
+public:
+    int doOperation(int num1, int num2)
+    {
+        return num1 - num2;
+    }
+};
+class OperationMultiply : public Strategy
+{
+public:
+    int doOperation(int num1, int num2)
+    {
+        return num1 * num2;
+    }
+};
+class OperationDivision : public Strategy
+{
+public:
+    int doOperation(int num1, int num2)
+    {
+        return num1 / num2;
+    }
+};
+class Context
+{
+private:
+    Strategy* strategy;
+
+public:
+    Context(Strategy* strategy)
+    {
+        this->strategy = strategy;
+    }
+
+    int executeStrategy(int num1, int num2)
+    {
+        return strategy->doOperation(num1, num2);
+    }
+};
+
+
+int main()
+{
+
+    Context* context = new Context(new OperationAdd());
+    cout<<"10 + 5 = " << context->executeStrategy(10, 5);
+
+    context = new Context(new OperationSubstract());
+    cout << "10 - 5 = " << context->executeStrategy(10, 5);
+
+    context = new Context(new OperationMultiply());
+    cout << "10 * 5 = " << context->executeStrategy(10, 5);
+
+    context = new Context(new OperationDivision());
+    cout << "10 / 5 = " << context->executeStrategy(10, 5);
+
+
+    return 0;
+
 }
 
